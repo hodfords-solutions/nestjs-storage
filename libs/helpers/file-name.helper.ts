76147e7eec83016e9999b2772a0942c26c2735fb
Nguyen Tien Dung <dung@hodfords.com>
2023-03-16 12:38:26 +0700

    feat: change the way to generate file name

--- libs/helpers/file-name.helper.ts
+++ libs/helpers/file-name.helper.ts
@@ -1,12 +1,17 @@
 import { v4 as uuidv4 } from 'uuid';
+import path from 'path';
 
-export function generateUniqueName(fileName = '') {
+export function generateUniqueName(filePath: string): string {
     const uuid = uuidv4();
     const maxLength = 255;
-    const expectedFileNameLength = fileName.length + uuid.length + 1;
+    const { dir, name, ext } = path.parse(filePath);
+    const expectedFileNameLength = name.length + uuid.length + 1 + ext.length;
+
     if (expectedFileNameLength > maxLength) {
-        fileName = fileName.substring(expectedFileNameLength - maxLength);
+        const fileName = `${uuid}-${name.substring(0, maxLength - uuid.length - ext.length - 1)}${ext}`;
+        return path.join(dir, fileName);
     }
 
-    return `${uuidv4()}-${fileName}`;
+    const fileName = `${uuid}-${name}${ext}`;
+    return path.join(dir, fileName);
 }
